bcff1770f92e1778d471555b233e2c312bddd4cd
Biplob <biplobhasan547@gmail.com>
2022-06-08 12:05:01 +0600

    add 2 more patterns

---
+++ src/components/Factory.tsx
@@ -0,0 +1,27 @@
+// Use a factory function in order to create objects
+
+// A function is a factory function when it returns a new object without the use of the new keyword!
+
+const createUser = ({ firstName, lastName, email }) => ({
+    firstName,
+    lastName,
+    email,
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    }
+  });
+  
+  const user1 = createUser({
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@doe.com"
+  });
+  
+  const user2 = createUser({
+    firstName: "Jane",
+    lastName: "Doe",
+    email: "jane@doe.com"
+  });
+  
+  console.log(user1);
+  console.log(user2);
